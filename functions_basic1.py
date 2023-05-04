6510cfafd09266d7f8e34b35d5974d9e1dafbf60
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-05-04 16:47:06 -0400

    final product

---
+++ functions_basic1.py
@@ -0,0 +1,40 @@
+#1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+#print 5
+
+#2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#print undefined bc number of days in the week...isn't defined
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+#print 5 and not 10 bc once the function is executed in the first return statement it exits the fuction
+
+
+#4
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+#print 5 for the same reason above the print statement will not be executed bc of the return statement above
+
+#5
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+#print 5 will print to console when the function is called by x there is no value so null/none will print
+
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+#print 3,5,8
+
+
