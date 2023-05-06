5f97ae8ca5d213cd4cf031570565f59a8016123d
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-05-06 11:09:36 -0400

    file creation

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-def multiply(num_list, num):
-    print(num_list, num)
-    for x in num_list:
-        print(x)
-        x *= num
-        print(num_list)
-    return num_list
-a = [2,4,10,16]
-b = multiply(a,5)
-print(b)
+def countdown(num):
+    new_list = []
+    for i in range(num, -1, -1):
+        new_list.append(i)
+    return new_list
+
+print(countdown(8))
