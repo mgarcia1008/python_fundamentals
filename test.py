24ebc397368183c4b9c71a65bc4abb548df5ccf2
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-05-07 11:55:38 -0400

    first commit

--- test.py
+++ test.py
@@ -5,3 +5,96 @@ def countdown(num):
     return new_list
 
 print(countdown(8))
+
+my_list = ["Thomas", "True", 10, "happy", "adrian"]
+
+for ind in range (len(my_list)):
+    print(my_list [ind])
+    print(ind)
+
+def generic_message():
+    print("Hello there")
+
+person = {"first": "Ada", "last": "Lovelace", "age": 42, "is_organ_donor": True}
+# Adds a new key value pair for email to person
+person["email"] = "alovelace@codingdojo.com"
+        
+# Changes person's "last" value to "Bobada"
+person["last"] = "Bobada"
+print(person)
+
+countries_so_far = {"Mexico": 1, "Morocco": 1}
+new_visits = ["Albania", "Mexico", "Togo", "Morocco"]
+    
+# To add Albania to the list
+countries_so_far["Albania"] = 1
+# To add 1 to the Mexico tally
+countries_so_far["Mexico"] += 1 # Changes Mexico tally to 2!
+# your code here to finish updating your travel log!
+
+
+person = {"first": "Ada", "last": "Lovelace", "age": 42, "is_organ_donor": True}
+print(person["first"])
+full_name = person["first"] + " " + person["last"]
+print(full_name)
+
+print(len(person))
+print(str(person))
+
+
+print(person.get("email"))
+print(person.get("email", "mgarciagmail.com"))
+print(person)
+
+my_dict = {"name": "Noelle", 
+           "language": "Python"
+           }
+for each_key in my_dict:
+    print(my_dict[each_key])
+
+capitals = {"Washington":"Olympia","California":"Sacramento","Idaho":"Boise","Illinois":"Springfield","Texas":"Austin","Oklahoma":"Oklahoma City","Virginia":"Richmond"}
+# another way to iterate through the keys
+for key in capitals.keys():
+     print(key)
+# output: Washington, California, Idaho, Illinois, Texas, Oklahoma, Virginia
+#to iterate through the values
+for val in capitals.values():
+     print(val)
+# output: Olympia, Sacramento, Boise, Springfield, Austin, Oklahoma City, Richmond
+#to iterate through both keys and values
+for key, val in capitals.items():
+     print(key, " = ", val)
+# output: Washington = Olympia, California = Sacramento, Idaho = Boise, etc
+
+"""
+This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
+Example: length_and_value(4,7) should return [7,7,7,7]
+Example: length_and_value(6,2) should return [2,2,2,2,2,2]"""
+
+def replicate_value(size_of_list, value_to_insert):
+    new_list = []
+    for this_value in range(size_of_list):
+        new_list.append(value_to_insert)
+    return new_list
+
+print(replicate_value(4,7))
+
+# List of dictionaries
+users = [
+    {"first": "Ada", "last": "Lovelace"}, # index 0
+    {"first": "Alan", "last": "Turing"}, # index 1
+    {"first": "Eric", "last": "Idle"} # index 2
+]
+# Dictionary of lists
+resume_data = {
+    #        	     0           1           2
+    "skills": ["front-end", "back-end", "database"],
+    #                0           1
+    "languages": ["Python", "JavaScript"],
+    #                0              1
+    "hobbies":["rock climbing", "knitting"]
+}
+
+
+print(resume_data["skills"][1])
+print(users)
